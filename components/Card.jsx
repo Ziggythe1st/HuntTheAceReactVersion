8600b83d891a105e0a38783e1ca84d05fb96c571
Ziggythe1st <Ziggythe1st@gmail.com>
2025-04-26 20:32:52 -0700

    Card flip logic working

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { flipCard } from "../features/cardSlice";
 
-function Card({ imagePath }) {
+function Card({ id, frontImage, backImage, isFlipped }) {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(flipCard(id)); // This flips the card
+  };
+
   return (
-    <div className="w-[157px] h-[220px] border-2 border-black m-2">
-      <img src={imagePath} alt="Card" className="w-full h-full object-cover" />
+    <div className="card m-2" onClick={handleClick}>
+      <div className={`card-inner ${isFlipped ? "flip" : ""}`}>
+        <div className="card-front">
+          <img
+            src={frontImage}
+            alt="Front"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="card-back">
+          <img
+            src={backImage}
+            alt="Back"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- components/CardGrid.jsx
+++ components/CardGrid.jsx
@@ -4,11 +4,18 @@ import Card from "./Card";
 
 function CardGrid() {
   const cards = useSelector((state) => state.cards.cards);
+  const backImg = "/public/card-back-blue.png";
 
   return (
     <div className="grid grid-cols-2 gap-4 border p-4">
       {cards.map((card) => (
-        <Card key={card.id} imagePath={card.imagePath} />
+        <Card
+          key={card.id}
+          id={card.id}
+          frontImage={card.imagePath}
+          backImage={backImg}
+          isFlipped={card.isFlipped}
+        />
       ))}
     </div>
   );
